dffa705e4128d744d6d28c1bae182fe074c5c09e
weibochang <1796907846@qq.com>
2018-05-08 11:56:17 +0800

    第一次python运算

---
+++ suanshu.py
@@ -0,0 +1,30 @@
+#coding:utf -8
+a=21
+b=10
+c=0
+
+c=a+b
+print("c=a+b :",c)
+
+c=a-b
+print("c=a-b :",c)
+
+c=a*b
+print ("c=a*b :",c)
+
+c=a/b
+print ("c=a/b :",c)
+
+c=a%b
+print ("c=a%b :",c)
+
+# 修改变量a、b、c
+a=2
+b=3
+c=a**b
+print ("c=a**b :",c)
+
+a=10
+b=5
+c=a//b
+print("c=a//b :", c)
