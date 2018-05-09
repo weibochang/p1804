3ae09d1865a591bce54f2ffeecdf3f68411a56a9
weibochang <1796907846@qq.com>
2018-05-09 23:13:01 +0800

    个人名片

---
+++ python_introduction/personal_card.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+print('-'*18)
+
+company = input('公司名称：')
+name = input('姓名(职位)：')
+phone = input('电话:')
+email = input('邮箱：')
+print('-'*18)
+
+print('公司的名称:%s\n你的名字:%s\n电话:%s\n邮箱:%s' %(company,name,phone,email))
