1819a2c384c824489407e9e55720824944354925
weibochang <1796907846@qq.com>
2018-05-10 09:50:30 +0800

    年龄的区间

---
+++ python_introduction/Nian.py
@@ -0,0 +1,14 @@
+age =  int(input('请输入你的年龄：'))
+
+if age > 85:
+    print('你是一个 老寿星')
+elif age > 60:
+    print('年过古稀了')
+elif age > 40:
+    print('不惑之年已经过去了')
+elif age > 20:
+    print('20出头的青年')
+elif age > 10:
+    print('你还小呢，在家好好待着，别乱跑')
+else:
+    print('你还不到十岁哦，在家好好带着吧')
